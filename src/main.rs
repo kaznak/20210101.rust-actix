f687e60500551a74f83237cc68164e3603d979a5
kaznak <kaznak.at.work@gmail.com>
2021-01-01 23:45:57 +0900

    src/main.rs: use trait

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+trait HelloStr {
+    fn hello_str(&self) -> &str;
+}
+
+impl HelloStr for &str {
+    fn hello_str(&self) -> &str {
+        return self;
+    }
+}
+
+impl HelloStr for String {
+    fn hello_str(&self) -> &str {
+        return &self;
+    }
+}
+
+fn hello(name: impl HelloStr) -> std::string::String {
+    let mut s = String::from("Hello, ");
+    s.push_str(name.hello_str());
+    s.push_str("!");
+    return s;
+}
+
 fn main() {
-    println!(
-        "{}",
-        (|name: &str| -> std::string::String {
-            let mut s = String::from("Hello, ");
-            s.push_str(name);
-            s.push_str("!");
-            return s;
-        })("world")
-    );
+    println!("{}", hello("world"));
+    println!("{}", hello(String::from("world")));
 }
