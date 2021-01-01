0927bc241d0b5de02a68abb00772914b368d40e0
kaznak <kaznak.at.work@gmail.com>
2021-01-01 22:01:51 +0900

    src/main.rs: as clausure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-fn hello(name: &str) -> std::string::String {
-    let mut s = String::from("Hello, ");
-    s.push_str(name);
-    s.push_str("!");
-    return s;
-}
-
 fn main() {
-    println!("{}", hello("world"));
+    println!(
+        "{}",
+        (|name: &str| -> std::string::String {
+            let mut s = String::from("Hello, ");
+            s.push_str(name);
+            s.push_str("!");
+            return s;
+        })("world")
+    );
 }
