734278e41eaa578c1a67860433ba73f5dbac165a
kaznak <kaznak.at.work@gmail.com>
2021-01-04 22:25:42 +0900

    src/main.rs: app state

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
-async fn index() -> impl Responder {
-    "Hello world!"
+// This struct represents state
+struct AppStateWithCounter {
+    app_name: String,
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
 }
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+
+    format!("Hello {}!", app_name) // <- response with app_name
+}
+
+async fn index_counter(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {}", counter) // <- response with count
+}
+
+async fn app_index() -> impl Responder {
+    "Hello world!"
 }
 
 #[post("/echo")]
@@ -20,12 +36,19 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        app_name: String::from("Actix-web"),
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
-            .service(web::scope("/app").route("/index.html", web::get().to(index)))
+            .service(web::scope("/app").route("/index.html", web::get().to(app_index)))
+            .app_data(counter.clone())
+            .service(index)
+            .route("/counter", web::get().to(index_counter))
     })
     .bind("127.0.0.1:8080")?
     .run()
